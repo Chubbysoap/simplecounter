504fa7a2bb86275d0411e703ebfc902b14595a87
Andrew Ledbetter <agledbetter795@gmail.com>
2025-06-18 10:05:13 -0500

    name change

--- src/main/scala/simplecounter.scala
+++ src/main/scala/simplecounter.scala
@@ -1,8 +1,9 @@
 package simplecounter
 
 import chisel3._
-import chipyard._
-import chipyard.config._
+import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.tilelink._
 
 // Simple 8-bit counter module
 class SimpleCounter extends Module {
@@ -20,15 +21,24 @@ class SimpleCounter extends Module {
 
 // Configuration for the counter
 class SimpleCounterConfig extends Config(
-  new freechips.rocketchip.subsystem.WithNBigCores(1) ++ // Use a single Rocket core
-    new chipyard.config.AbstractConfig
-) {
-  override def generator = (p, s) => new SimpleCounter()(p, s)
+  new WithNBigCores(1) ++ // Use a single Rocket core
+    new freechips.rocketchip.config.Config((site, here, up) => {
+      case BuildTop => (p: Parameters) => new SimpleCounterTop()(p)
+    })
+)
+
+// Top-level module integrating SimpleCounter with Rocket Chip
+class SimpleCounterTop(implicit p: Parameters) extends RocketSubsystem {
+  override lazy val module = new SimpleCounterTopModule(this)
 }
 
-// Generator to elaborate the design
-class SimpleCounterGenerator extends chipyard.Generator {
-  def elaborate(p: freechips.rocketchip.config.Parameters): chisel3.Module = {
-    new SimpleCounter()(p)
-  }
+class SimpleCounterTopModule(outer: SimpleCounterTop) extends RocketSubsystemModuleImp(outer) {
+  val counter = Module(new SimpleCounter)
+  // Connect counter inputs and outputs (example: tie enable to a constant or external input)
+  counter.io.en := true.B // Example: always enabled; modify as needed
+  // Expose count to external IO or connect to Rocket Chip's debug interface
+  val io = IO(new Bundle {
+    val count = Output(UInt(8.W))
+  })
+  io.count := counter.io.count
 }
